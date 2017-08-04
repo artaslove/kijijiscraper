2c78aca7f327963faee2c01c62fb4b973f5ad9bb
Tony <bonafide@martica.org>
2017-08-04 11:04:22 -0700

    Add files via upload

--- kijijispider.py
+++ kijijispider.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import scrapy
 import json
 import re
@@ -8,45 +7,35 @@ from datetime import datetime
 
 class KijijiSpider(scrapy.Spider):
 	name = 'KijijiSpider'
-	start_urls = ['http://www.kijiji.ca/b-programmer-computer-jobs/vancouver/c54l1700287',
-		      'http://www.kijiji.ca/b-programmer-computer-jobs/nelson/c54l1700226',
-		      'http://www.kijiji.ca/b-programmer-computer-jobs/victoria-bc/c54l1700173',
-		      'http://www.kijiji.ca/b-programmer-computer-jobs/kelowna/c54l1700228',
-		      'http://www.kijiji.ca/b-programmer-computer-jobs/cranbrook/c54l1700224',
-		      'http://www.kijiji.ca/b-programmer-computer-jobs/kamloops/c54l1700227',
-		      'http://www.kijiji.ca/b-programmer-computer-jobs/vernon/c54l1700229',
-		      'http://www.kijiji.ca/b-programmer-computer-jobs/gta-greater-toronto-area/c54l1700272']
-	# todo: add additional places
+	start_urls = ['http://www.kijiji.ca/b-programmer-computer-jobs/british-columbia/c54l9007']
 
 	def parse(self, response):
 		for href in response.css('.description a::attr(href)'):
-			full_url = response.urljoin(href.extract())
-			request = scrapy.Request(full_url, callback=self.parse_job)
-			yield request
+			site = href.extract()
+			# Ignoring the indeed.com matches for now
+			if site[:1] == '/':
+				yield scrapy.Request(response.urljoin(site), callback=self.parse_job)
+		# next page
+		hxs = Selector(response)
+		next = hxs.xpath('//div[@class="pagination"]/a[@title="Next"]/@href').extract_first()
+		if next is not None:
+			yield scrapy.Request(response.urljoin(next), callback=self.parse)
 
 	def parse_job(self, response):
 		hxs = Selector(response)
 		adid = re.sub('^.*/', '', response.url)
-		request = FormRequest('http://www.kijiji.ca/j-vac-inc-get.json',formdata={'adId': adid},callback=self.parse_json,method='GET')
-		tempdate = datetime.strptime(response.css('.ad-attributes td::text').extract()[0], '%d-%b-%y')
-		request.meta['date'] = tempdate.strftime('%Y-%m-%d')
-		request.meta['address'] = response.css('.ad-attributes td::text').extract()[1]
-		request.meta['content'] = hxs.xpath('//div[@id="UserContent"]').extract()
-		request.meta['title'] = hxs.xpath('/html/head/title/text()').extract()
-		request.meta['link'] = response.url
-		request.meta['adid'] = adid
-		return request
-
-
-	def parse_json(self, response):
-		data = json.loads(response.body)
+		adid = re.sub('\?.*$', '', adid)
+		thedate = hxs.xpath('//time/@datetime').extract_first()
+		address = hxs.xpath('//span[contains(@class,"address-")]/text()').extract_first()
+		content = hxs.xpath('//div[contains(@class,"descriptionContainer-")]/div[1]').extract_first()
+		title = hxs.xpath('//h1[contains(@class,"title-")]/text()').extract_first()
+		link = response.url
 		yield {
-			'date': response.meta['date'],
-			'address': response.meta['address'],
-			'content': response.meta['content'],
-			'link': response.meta['link'],
-			'title': response.meta['title'],
-			'adid': response.meta['adid'],
-			'visits': data['numVisits'],
+			'date': thedate,
+			'address': address,
+			'content': content,
+			'link': link,
+			'title': title,
+			'adid': adid
 		}
 
